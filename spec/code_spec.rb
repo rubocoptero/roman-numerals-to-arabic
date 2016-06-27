65533c27acd3a4b1fc5ad01f8bbed2aa31f9cff7
Ruben Anton <rubocoptero@gmail.com>
2016-06-27 08:30:28 +0200

    V is 5

--- spec/code_spec.rb
+++ spec/code_spec.rb
@@ -1,16 +1,26 @@
 require_relative './../src/code'
 
 describe 'Roman Numerals' do
+  # Initial
   it 'I is 1' do
     arabic = roman_to_arabic('I')
 
     expect(arabic).to eq(1)
   end
 
+  # Triangulation
   it 'III is 3' do
     arabic = roman_to_arabic('III')
 
     expect(arabic).to eq(3)
+  end
+
+
+  # Adding V
+  it 'V is 5' do
+    arabic = roman_to_arabic('V')
+
+    expect(arabic).to eq(5)
   end
 end
 
--- src/code.rb
+++ src/code.rb
@@ -1,4 +1,6 @@
 def roman_to_arabic(roman)
+  return 5 if roman == 'V'
+
   tokens = roman.chars
 
   tokens.size
